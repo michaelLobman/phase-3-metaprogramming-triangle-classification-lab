00ad81bcd0b38b7dd8aaf650e208098077fbcaa1
Michael Lobman <michael.lobman@gmail.com>
2022-01-12 20:43:37 -0500

    commit tree f00b96dd15fa3341ed339cf2b7dd4b0ff0baa8c8

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
 class Triangle
-  # write code here
+
+  attr_reader :s1, :s2, :s3, :side_array
+
+
+  def initialize(s1, s2, s3)
+    @s1 = s1
+    @s2 = s2
+    @s3 = s3
+    @side_array = [s1, s2, s3]
+  end
+
+  def valid_triangle?
+    if self.side_array.find { |value| value <= 0 }
+      raise TriangleError
+    elsif self.s1 + self.s2 <= self.s3 || self.s1 + self.s3 <= self.s2 || self.s2 + self.s3 <= self.s1
+      raise TriangleError
+    end
+  end
+
+  def kind
+    self.valid_triangle?
+  
+    if self.side_array.uniq.length == 1
+      :equilateral
+    elsif self.side_array.uniq.length == 2
+      :isosceles
+    else 
+      :scalene
+    end
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
